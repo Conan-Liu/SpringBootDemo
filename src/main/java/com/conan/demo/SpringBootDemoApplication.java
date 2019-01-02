175799c5d3f544efa368673da72a8c86ea320c92
liufeiqiang <liu.feiqiang@puscene.com>
2019-01-02 15:10:55 +0800

    Spring Boot 项目

---
+++ src/main/java/com/conan/demo/SpringBootDemoApplication.java
@@ -0,0 +1,22 @@
+package com.conan.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello Spring Boot";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoApplication.class, args);
+    }
+
+}
+
