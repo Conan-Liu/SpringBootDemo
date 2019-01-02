2f95489c6fdfd35a2569cdafdcc0a5e27ee94eca
liufeiqiang <liufeiqiang@aspirecn.com>
2019-01-02 20:57:53 +0800

    update

--- src/main/java/com/conan/demo/SpringBootDemoApplication.java
+++ src/main/java/com/conan/demo/SpringBootDemoApplication.java
@@ -1,11 +1,13 @@
 package com.conan.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@MapperScan("com.conan.demo.mapper")
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
